84b2a9296ba362c34f38e1095435efa7048bd20e
youmari <youmari300@gmail.com>
2021-11-24 09:07:33 +0100

    Change diable eslint in the main js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
+/* eslint-disable  max-classes-per-file */
 const allbooksContainer = document.querySelector('.books-container');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const addBtn = document.querySelector('#add-btn');
 
-class BookDetails { /*eslint-disable*/
-  constructor(title, author) {/*eslint-disable*/
-    this.title = title;  /*eslint-disable*/
-    this.author = author; /*eslint-disable*/
+class BookDetails {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-}/*eslint-disable*/
-  class Library {
+}
+class Library {
   constructor() {
     this.booksData = [];
   }
-  
+
   removeFromCollection(book) {
     const newData = this.booksData.filter((element) => element !== book);
     this.booksData = newData;
   }
-  
+
   getBooksFromLocal() {
     const retrievedBooks = JSON.parse(localStorage.getItem('books'));
     this.booksData = retrievedBooks;
   }
-  
+
   storeBooksInLocal() {
     localStorage.setItem('books', JSON.stringify(this.booksData));
   }
-  
+
   removeBook(removeBtn, book) {
     removeBtn.addEventListener('click', () => {
       removeBtn.parentElement.remove();
@@ -35,13 +36,13 @@ class BookDetails { /*eslint-disable*/
       this.storeBooksInLocal();
     });
   }
-  
+
   createElement(book) {
     const bookContainer = document.createElement('article');
     const bookTitle = document.createElement('h4');
     const bookAuthor = document.createElement('h5');
     const removeBtn = document.createElement('button');
-    
+
     allbooksContainer.append(bookContainer);
     bookContainer.classList.add('book-info-conatiner');
     bookContainer.append(bookTitle, bookAuthor, removeBtn);
@@ -49,12 +50,12 @@ class BookDetails { /*eslint-disable*/
     bookAuthor.classList.add('author-book');
     removeBtn.classList.add('remove-btn');
     removeBtn.textContent = 'Remove';
-    
+
     bookTitle.textContent = book.title;
     bookAuthor.textContent = book.author;
     this.removeBook(removeBtn, book);
   }
-  
+
   addBook() {
     addBtn.addEventListener('click', (e) => {
       if (titleInput.value === '' || authorInput.value === '') {
@@ -69,7 +70,7 @@ class BookDetails { /*eslint-disable*/
       }
     });
   }
-  
+
   display() {
     window.addEventListener('load', (e) => {
       if (localStorage.getItem('books') === null) {
