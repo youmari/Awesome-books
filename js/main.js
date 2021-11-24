cc59a72006b2f34f4218c6a063607998fbac27ba
youmari <youmari300@gmail.com>
2021-11-24 01:02:09 +0100

    Fix eslint errors

--- js/main.js
+++ js/main.js
@@ -5,11 +5,23 @@ const authorInput = document.querySelector('#author');
 const addBtn = document.querySelector('#add-btn');
 let booksData = [];
 
-function bookDetails(title, author) {
+function BookDetails(title, author) {
   this.title = title;
   this.author = author;
 }
 
+const removeFromCollection = (book) => {
+  const newData = booksData.filter((element) => element !== book);
+  booksData = newData;
+};
+
+const getBooksFromLocal = () => {
+  const retrievedBooks = JSON.parse(localStorage.getItem('books'));
+  booksData = retrievedBooks;
+};
+const storeBooksInLocal = () => {
+  localStorage.setItem('books', JSON.stringify(booksData));
+};
 
 const removeBook = (removeBtn, book) => {
   removeBtn.addEventListener('click', () => {
@@ -17,14 +29,14 @@ const removeBook = (removeBtn, book) => {
     removeFromCollection(book);
     storeBooksInLocal();
   });
-}
+};
 
 const createElement = (book) => {
   const bookContainer = document.createElement('article');
   const bookTitle = document.createElement('h4');
   const bookAuthor = document.createElement('h5');
   const removeBtn = document.createElement('button');
-  
+
   allbooksContainer.append(bookContainer);
   bookContainer.classList.add('book-info-conatiner');
   bookContainer.append(bookTitle, bookAuthor, removeBtn);
@@ -32,47 +44,32 @@ const createElement = (book) => {
   bookAuthor.classList.add('author-book');
   removeBtn.classList.add('remove-btn');
   removeBtn.textContent = 'Remove';
-  
+
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
-  removeBook(removeBtn,book);
-  console.log('here')
-}
+  removeBook(removeBtn, book);
+};
 
 const addBook = () => {
   addBtn.addEventListener('click', (e) => {
     if (titleInput.value === '' || authorInput.value === '') {
       e.preventDefault();
     } else {
-      let book = new bookDetails(titleInput.value, authorInput.value);
+      const book = new BookDetails(titleInput.value, authorInput.value);
       booksData.push(book);
       createElement(book);
       storeBooksInLocal();
       titleInput.value = '';
       authorInput.value = '';
     }
-  })
-}
-
-const removeFromCollection = (book) => {
-  let newData = booksData.filter((element) => element !== book);
-  booksData = newData;
-}
-
-const storeBooksInLocal = () => {
-  localStorage.setItem('books', JSON.stringify(booksData));
-}
-
-const getBooksFromLocal = () => {
-  let retrievedBooks = JSON.parse(localStorage.getItem('books'));
-  booksData = retrievedBooks;
-}
+  });
+};
 
 window.addEventListener('load', () => {
   getBooksFromLocal();
   booksData.forEach((book) => {
     createElement(book);
-  })
-})
-addBook();
+  });
+});
 
+addBook();
